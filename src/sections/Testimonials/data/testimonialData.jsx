820e5d17d5022c2288010e04c3451e99d19a7d70
Atharva Jagtap <atharvauni2021@gmail.com>
2025-05-14 16:10:01 +0400

    Added case studies

--- src/sections/Testimonials/data/testimonialData.jsx
+++ src/sections/Testimonials/data/testimonialData.jsx
@@ -7,7 +7,7 @@ export const testimonialData = [
     testimonial: "I am writing to highly recommend Atharva Jagtap, who worked as a Software Engineer Intern at Getmax.ae. During his time, Atharva demonstrated exceptional technical proficiency, a strong work ethic, and a remarkable ability to collaborate effectively within our team. Beyond his technical expertise, Atharva consistently exhibited strong communication and leadership skills. He was able to clearly articulate his ideas, work closely with cross-functional teams, and ensure smooth project execution. His professionalism, combined with his eagerness to learn and adapt, makes him an invaluable asset to any organization. I am confident that Atharva will continue to excel in his future endeavors and strongly recommend him for any role that seeks a highly skilled, dedicated, and collaborative individual.",
     shortTestimonial: "I am writing to highly recommend Atharva Jagtap, who worked as a Software Engineer Intern at Getmax.ae. During his time...",
     color: "#8A7F8D", // Mercury
-    caseStudyLink: "/case-studies/getmax-ai-integration.pdf",
+    caseStudyLink: "https://getmax.ae/",
     contactReference: {
       linkedin: "https://www.linkedin.com/in/rightpankaj/"
     }
@@ -20,7 +20,7 @@ export const testimonialData = [
     testimonial: "I’ve had the privilege of working alongside Atharva Jagtap as a co-founder of NxtMeals, and I can confidently say that his technical expertise and problem-solving mindset have been instrumental in bringing our vision to life. From building our Wix website to developing an interactive prototype, Atharva has played a crucial role in translating our ideas into a tangible product that resonates with our users. His ability to navigate complex development challenges, quickly iterate on user feedback, and maintain a keen eye for design and functionality has significantly accelerated our progress. Beyond his technical skills, Atharva is a dedicated, detail-oriented, and resourceful founder who thrives in fast-paced, problem-solving environments. Whether it’s tackling unexpected roadblocks or optimizing workflows, his adaptability and perseverance ensure that our product continues to evolve.",
     shortTestimonial: "I’ve had the privilege of working alongside Atharva Jagtap as a co-founder of NxtMeals, and I can confidently say that his technical expertise...",
     color: "#F7DC6F", // Venus
-    caseStudyLink: "/case-studies/nxtmeals-app-development.pdf",
+    caseStudyLink: "https://linktr.ee/nxtmeals",
     contactReference: {
       linkedin: "https://www.linkedin.com/in/siddharthsj/"
     }
